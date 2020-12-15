a62459e669d55e3011fda0be5c0dc1a1b076db0b
Pavel <t329023@gmail.com>
2020-12-15 15:54:28 +0800

    add animation

--- app/src/main/java/com/pavel2309/hskfivereadingtrainer/HskFiveReadingTrainer.kt
+++ app/src/main/java/com/pavel2309/hskfivereadingtrainer/HskFiveReadingTrainer.kt
@@ -3,6 +3,7 @@ package com.pavel2309.hskfivereadingtrainer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
 class HskFiveReadingTrainer : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
